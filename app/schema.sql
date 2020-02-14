fb2f0a8196668319a541d2fee3f110e042367f95
JerryNyoike <jngugi31@gmail.com>
2020-02-14 09:17:56 +0300

    Add tables for all entities and enforce relations

--- app/schema.sql
+++ app/schema.sql
@@ -9,3 +9,39 @@ CREATE TABLE client(
 	phone_number INT(20) UNIQUE,
 	name VARCHAR(36)
 );
+
+CREATE TABLE producer(
+	producer_id BINARY(16) NOT NULL UNIQUE,
+	email VARCHAR(36) NOT NULL UNIQUE,
+	name VARCHAR(36) NOT NULL,
+	phone_number VARCHAR(36) NOT NULL UNIQUE,
+	pwd VARCHAR(36) NOT NULL,
+
+	PRIMARY KEY(producer_id)
+);
+
+CREATE TABLE beat(
+	beat_id BINARY(16) UNIQUE NOT NULL,
+	producer_id BINARY(16) NOT NULL,
+	name VARCHAR(36) NOT NULL,
+	genre VARCHAR(36) NOT NULL,
+	address VARCHAR(36) NOT NULL,
+	preview_address VARCHAR(36) NOT NULL,
+	lease_price INT(11) NOT NULL,
+	selling_price INT(11),
+
+	PRIMARY KEY(beat_id),
+	FOREIGN KEY(producer_id) REFERENCES producer(producer_id)
+	ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE payment(
+	payment_id BINARY(16) UNIQUE NOT NULL,
+	beat_id BINARY(16) NOT NULL,
+	amount INT(11) NOT NULL,
+	client_id BINARY(16) NOT NULL,
+
+	PRIMARY KEY(payment_id),
+	FOREIGN KEY beat_id REFERENCES beat(beat_id)
+	ON UPDATE CASCADE ON DELETE CASCADE
+);
