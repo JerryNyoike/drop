104ebd59b6f0ff52b492baa423c0040bedd01b5b
JerryNyoike <jngugi31@gmail.com>
2020-02-16 12:15:11 +0300

    Register authentication blueprint to the app and make a call to init_app to setup the flask app

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+import os
+import pymysql
+from . import auth, db
+from flask import Flask
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+            DB='dropbeats'
+            , DB_HOST='localhost'
+            , DB_USER='droppy'
+            , DB_PASS='dropitlikeitshot'
+            , CURSOR=pymysql.cursors.DictCursor
+            , SCRT='topsecrettochangelater')
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    db.init_app(app)
+
+    app.register_blueprint(auth.bp)
+
+    return app
