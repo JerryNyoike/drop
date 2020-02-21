5604dbd70de3e32731b9902dadbb4d3e9a60b45a
JerryNyoike <jngugi31@gmail.com>
2020-02-21 15:00:04 +0300

    Refactor uploadBeat to a flask Blueprint

--- app/uploadBeat.py
+++ app/uploadBeat.py
@@ -1,48 +1,41 @@
-#pip install flask-mysql
 import pymysql
-from flask import Flask, make_response, request
+from flask import Blueprint, make_response, request, current_app
 
 from . import db
+from .auth import is_logged_in
 
-app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'penguin'
-app.config['MYSQL_PASSWORD'] = 'penguin'
-app.config['MYSQL_DB'] = 'MULTIMEDIA'
-app.config['MYSQL_CURSOR'] = pymysql.cursors.DictCursor
+bp = Blueprint('uploadBeat', __name__, url_prefix="/beat")
 
-@app.route('/')
-def index():
-    return 'This is the default homepage'
 
-@app.route('/redirectURL', methods=['POST'])
+@bp.route('/redirectURL', methods=['POST'])
 def insertBeat():
     #Check for JSON
     if request.is_json():
-        beatInfo = request.get_json()
+        producer = request['token']
+        if is_logged_in(producer):
+            if request.files['beat'] is not None:
+                beatInfo = request.get_json()
 
-        producerID = beatInfo['producerID']
-        beatName = beatInfo['name']
-        beatGenre = beatInfo['genre']
-        beatFilePath = beatInfo['filePath']
-        beatLeasePrice = beatInfo['leasePrice']
-        beatSellingPrice = beatInfo['sellingPrice']
+                producerID = beatInfo['producerID']
+                beatName = beatInfo['name']
+                beatGenre = beatInfo['genre']
+                beatFilePath = beatInfo['filePath']
+                beatLeasePrice = beatInfo['leasePrice']
+                beatSellingPrice = beatInfo['sellingPrice']
 
-        insertBeatQuery = "INSERT INTO beat VALUES ({}, {}, {}, {}, {}, {}, {}".format(producerID, beatName, beatGenre, beatFilePath, beatLeasePrice, beatSellingPrice)
+                insertBeatQuery = "INSERT INTO beat VALUES ({}, {}, {}, {}, {}, {}, {}".format(producerID, beatName, beatGenre, beatFilePath, beatLeasePrice, beatSellingPrice)
 
-        databaseConnection = db.get_db()
-        databaseCursor = databaseConnection.cursor()
+                databaseConnection = db.get_db()
+                databaseCursor = databaseConnection.cursor()
 
-        databaseCursor.execute(insertBeatQuery)
-        databaseConnection.commit()
+                databaseCursor.execute(insertBeatQuery)
+                databaseConnection.commit()
 
-        return "The beat has been successfully uploaded"
-
+                return make_response({'status': 1, 'message': "The beat has been successfully uploaded"}, 200)
+            else:
+                return make_response({'status': 0, 'message': 'Upload file cannot be null.'}, 400)
+        else:
+            return make_response({'status': 0, 'message': 'Must be logged in to perform this request.'}, 404)
     else:
         return make_response({'status': 0, 'message': 'Invalid data.'}, 400)
-
-
-
-if __name__ == '__main':
-    app.run(debug=True)
