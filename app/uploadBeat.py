0473b5271d05362d63227ac88588966bff0f9e3c
JerryNyoike <jngugi31@gmail.com>
2020-02-29 18:41:16 +0300

    Add endpoint for deleting a beat

--- app/uploadBeat.py
+++ app/uploadBeat.py
@@ -39,3 +39,40 @@ def insertBeat():
             return make_response({'status': 0, 'message': 'Must be logged in to perform this request.'}, 404)
     else:
         return make_response({'status': 0, 'message': 'Invalid data.'}, 400)
+
+
+@bp.route('delete', methods=['POST'])
+def delete_beat():
+    ''' remove the beat with beat_id from the database'''
+    if request.content_type is 'application/json':
+        if is_logged_in(request['tkn']):
+            if beat_exists():
+                #logic here
+                cur = get_db().cursor()
+                query = "DELETE FROM beat WHERE beat_id={} LIMIT 1"
+                result = cur.execute(query)
+                cur.commit()
+                if result == 1:
+                    response = make_response({'status':1, 'message': 'Successfully deleted beat'})
+                    return resp
+                else:
+                    resp = make_response({'status':0, 'message': 'Error occurred when deleting the beat'}, 404)
+                    return resp
+            else:
+                return make_response({'status':0, 'message': 'Beat does not exists or has been deleted.'} , 404)
+        else:
+            return make_response({'status': 0, 'message': 'Must be logged in to complete this request.'}, 404)
+    else:
+        return make_response({'status':0, 'message': 'Invalid content type.'}, 404)
+
+
+def beat_exists(beat_id):
+    cur = get_db().cursor()
+    query = "SELECT name FROM beat WHERE beat_id={} LIMIT 1".format(beat_id)
+    cur.execute(query)
+
+    result = cur.fetch_one()
+    if result is not None:
+        return True
+
+    return False
