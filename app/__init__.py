5de523f2e1946bfc4dafcaa74bb8660730f65162
Brackie <muimikieleko@gmail.com>
2020-04-13 00:58:40 +0300

    added code to create system wide directories

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-import os
+from os import path, makedirs
 import pymysql
 from . import auth, db, beat
 from flask import Flask
@@ -12,7 +12,22 @@ def create_app(test_config=None):
     app.config.from_pyfile("config.py", silent=True)
 
     try:
-        os.makedirs(app.instance_path)
+    
+    except OSError as e:
+    print(e)
+    try:
+        if not path.exists(beat_path):
+            makedirs(app.instance_path)
+
+        if not path.exists(current_app.config['BEAT_DIR']):
+            makedirs(current_app.config['BEAT_DIR'])
+
+        if not path.exists(current_app.config['PREVIEW_DIR']):
+            makedirs(current_app.config['PREVIEW_DIR'])
+
+        if not path.exists(current_app.config['TEMP_FOLDER']):
+            makedirs(current_app.config['TEMP_FOLDER'])
+
     except OSError:
         pass
 
