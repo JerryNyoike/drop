61e7145e467966a7b1d4445717477c798c5ef045
JerryNyoike <jngugi31@gmail.com>
2020-04-27 13:15:57 +0300

    Use app instead of current app since we are not in the current app context

--- app/__init__.py
+++ app/__init__.py
@@ -10,24 +10,18 @@ def create_app(test_config=None):
     app.config.from_mapping(
         ALLOWED_EXTENSIONS=['mp3', 'flac', 'ogg', 'wav', 'm4a']
     )
-    app.config.from_pyfile("../config.py", silent=True)
+    app.config.from_pyfile("config.py", silent=True)
+
 
     try:
-    
-    except OSError as e:
-    print(e)
-    try:
-        if not path.exists(beat_path):
-            makedirs(app.instance_path)
+        if not path.exists(app.config['BEAT_DIR']):
+            makedirs(app.config['BEAT_DIR'])
 
-        if not path.exists(current_app.config['BEAT_DIR']):
-            makedirs(current_app.config['BEAT_DIR'])
+        if not path.exists(app.config['PREVIEW_DIR']):
+            makedirs(app.config['PREVIEW_DIR'])
 
-        if not path.exists(current_app.config['PREVIEW_DIR']):
-            makedirs(current_app.config['PREVIEW_DIR'])
-
-        if not path.exists(current_app.config['TEMP_FOLDER']):
-            makedirs(current_app.config['TEMP_FOLDER'])
+        if not path.exists(app.config['TEMP_FOLDER']):
+            makedirs(app.config['TEMP_FOLDER'])
 
     except OSError:
         pass
