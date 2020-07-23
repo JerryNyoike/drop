298a43d006ac2afb7e88988cff49b1b77dd6f1cb
Brackie <muimikieleko@gmail.com>
2020-07-23 17:25:53 +0300

    added catch for expired token at is_logged_in

--- app/helpers.py
+++ app/helpers.py
@@ -1,3 +1,4 @@
+from flask import current_app
 from hashlib import sha256
 from os.path import join
 from datetime import datetime
@@ -9,12 +10,19 @@ def log_error(error, log_file):
         f.write(datetime.now().strftime("%d/%m/%Y %H:%M:%S") + ": " + error + "\n")
         f.close()
 
+
 def is_logged_in(token):
     try:
         return jwt.decode(token, current_app.config['SCRT'], algorithm='HS256')
     except jwt.exceptions.DecodeError as e:
         log_error("At is_logged_in, " + str(e), "jwt_error_logs.txt")
-        return make_response({'status': 0, 'message': 'Must be logged in to perform action'}, 401)
+        return None
+    except jwt.exceptions.ExpiredSignatureError as e:
+        log_error("At is_logged_in, " + str(e), "jwt_error_logs.txt")
+        return None
+
+    return None
+
 
 def string_hash(to_hash):
     return sha256(to_hash.encode()).hexdigest()
