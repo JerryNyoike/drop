6f7083b9c955bb159014ce134bb852610efc8e0a
Brackie <muimikieleko@gmail.com>
2020-07-23 17:30:28 +0300

    Moved some views to category bp

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template, redirect, session
+from flask import Blueprint, current_app, render_template, url_for, redirect, session, send_from_directory
+from markupsafe import escape
+from werkzeug.utils import secure_filename
+from werkzeug.exceptions import HTTPException
+from . import db
 
 
 bp = Blueprint('routes', __name__, url_prefix='/')
@@ -9,26 +13,77 @@ def index():
 	return render_template('index.html', page="Home"), 200
 
 
-@bp.route('/explore', methods=['GET'])
-def explore():
-	return render_template('explore.html', page="Explore"), 200
+@bp.route('discover', methods=['GET'])
+def discover():
+	return render_template('discover.html', page="Discover"), 200
 
 
-@bp.route('/about', methods=['GET'])
+@bp.route('suggestions', methods=['GET'])
+def suggestions():
+    return render_template('suggestions.html', page="For You"), 200
+
+
+@bp.route('about', methods=['GET'])
 def about():
 	return render_template('about.html', page="About"), 200
 
 
-@bp.route('/produce', methods=['GET'])
+@bp.route('produce', methods=['GET'])
 def produce():
 	return render_template('produce.html', page="Produce"), 200
 
 
-@bp.route('/contacts', methods=['GET'])
+@bp.route('contacts', methods=['GET'])
 def contacts():
 	return render_template('contacts.html', page="Contacts"), 200
 
 
-@bp.route('/cart', methods=['GET'])
-def cart():
-	return render_template('cart.html', page="Cart"), 200
+@bp.route('premium', methods=['GET'])
+def premium():
+    return render_template('premium.html', page="Try Premium"), 200
+
+@bp.route('producer/<producer_id>', methods=['GET'])
+def producer_profile(producer_id):
+    crumbs = [
+        {"name": "Home", "url": url_for('.index')}
+    ]
+
+    producer_query = '''SELECT (BIN_TO_UUID(producer_id)) producer_id, profile_image, email, name, phone_number 
+    FROM producer WHERE producer_id = UUID_TO_BIN("{}")'''.format(escape(producer_id))
+
+    beats_query = '''SELECT (BIN_TO_UUID(beat_id)) beat_id, name, upload_date FROM beat WHERE 
+    producer_id = UUID_TO_BIN("{}")'''.format(escape(producer_id))
+
+    conn = db.get_db()
+    cur = conn.cursor()
+
+    cur.execute(producer_query) 
+    producer = cur.fetchone()
+
+    cur.execute(beats_query)
+    beats = cur.fetchall()
+
+    if not producer:
+        return render_template('producer_profile.html'), 404
+
+    return render_template('producer_profile.html', page=producer["name"], crumbs=crumbs, producer=producer, beats=beats), 200
+
+
+@bp.route('resource/beat/<filename>', methods=['GET'])
+def uploaded_beat(filename):
+    return send_from_directory(current_app.config['BEAT_DIR'], secure_filename(filename))
+
+
+@bp.route('resource/preview/<filename>', methods=['GET'])
+def uploaded_preview(filename):
+    return send_from_directory(current_app.config['PREVIEW_DIR'], secure_filename(filename))
+
+
+@bp.route('resource/images/<filename>', methods=['GET'])
+def uploaded_image(filename):
+    return send_from_directory(current_app.config['PHOTO_DIR'], secure_filename(filename))
+
+
+@bp.errorhandler(HTTPException)
+def error_handler(e):
+    return render_template("error_handler.html", page="Not Found", error=e.code, message=e.name), e.code
