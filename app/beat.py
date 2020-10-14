e58038cfb7051f3307f22df744f6c5576f225c55
JerryNyoike <jngugi31@gmail.com>
2020-10-14 09:50:09 +0300

    Add a function for extracting the beat features

--- app/beat.py
+++ app/beat.py
@@ -41,6 +41,31 @@ def crop_beat(beat_path):
 
         preview.export(filename, format='mp3')
 
+def extract_features(beat_path):
+    ''' This function extracts features required by the AI model to generate genres
+    '''
+    with file as open('beat_data.csv', 'w', newline=''):
+        writer = csv.writer(file)
+        writer.writerow(header)
+
+        genres = 'blues classical country disco hiphop jazz metal pop reggae rock'.split()
+        for filename in os.listdir(f'./instance/previews/{g}'):
+            songname = f'./instance/previews/{g}/{filename}'
+            y, sr = librosa.load(songname, mono=True, duration=30)
+            chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
+            spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
+            spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
+            rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+            zcr = librosa.feature.zero_crossing_rate(y)
+            mfcc = librosa.feature.mfcc(y=y, sr=sr)
+            to_append = f'{filename} {np.mean(chroma_stft)} {np.mean(rmse)} {np.mean(spec_cent)} {np.mean(spec_bw)} {np.mean(rolloff)} {np.mean(zcr)}'    
+            for e in mfcc:
+                to_append += f' {np.mean(e)}'
+                to_append += f' {g}'
+                file = open('beat_data.csv', 'a', newline='')
+                with file:
+                    writer = csv.writer(file)
+                    writer.writerow(to_append.split())
 
 @bp.route('beat/upload', methods=['POST'])
 def insertBeat():
