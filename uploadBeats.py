85e2fd79950ebd618421db13ab02887017b5534f
JerryNyoike <jngugi31@gmail.com>
2020-10-22 14:39:23 +0300

    Add a script to upload the beats from the gtzan dataset

---
+++ uploadBeats.py
@@ -0,0 +1,31 @@
+import requests
+import random
+import os
+from functools import partial
+
+uploadUrl = 'http://127.0.0.1/producer/upload'
+headers = { 'Authorization': 'Bearer ' }
+
+def upload(beatDir, beat):
+    def beat_category(beat_name):
+        return beat_name.split('0')[0]
+
+    beatPath = os.path.join(beatDir, beat)
+    files = {'file': open(beatPath, 'rb')}
+    payload = {
+            'name': beat,
+            'category': beat_category,
+            'leasePrice': random.randrange(200, 300),
+            'sellingPrice': random.randrange(500, 1000)
+            }
+    resp = requests.post(uploadUrl, files=files, data=payload, headers = headers)
+    return resp.status_code
+
+
+def uploadBeats(beat_directory_path):
+    upload_part = partial(upload, beat_directory_path)
+    responses = list()
+    with os.list_dir(beatDirectoryPath) as beat_list:
+        responses = list(map(upload_part, beat_list))
+
+uploadBeats(os.path.join(os.getcwd(), "static", "test-beats"))
